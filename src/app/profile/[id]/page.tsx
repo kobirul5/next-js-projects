a735dffae632b0de6cae808a9baa97761d71d887
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-03-05 22:34:31 +0600

    added id

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function Id() {
+export default function Id({params}: any) {
   return (
-    <div>Id</div>
+    <div>
+      <h2  className='text-2xl text-center mt-20'>Profile Id: <span className='bg-green-500 rounded p-3 text-black'>{params.id}</span></h2>
+    </div>
   )
 }
